541d73311bdcc8220f94231fd27fb34b6197730a
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2023-12-06 12:18:49 +0100

    Improved Testcontainers demo
    This commit uses the connection details abstraction that also offers
    Testcontainers support. It adds a single dependency and the use of
    `@ServiceConnection` to bind the container to the application.

--- src/test/java/com/example/demo/CustomerRepositoryTests.java
+++ src/test/java/com/example/demo/CustomerRepositoryTests.java
@@ -9,8 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,14 +19,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CustomerRepositoryTests {
 
 	@Container
+	@ServiceConnection
 	static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.1-alpine");
-
-	@DynamicPropertySource
-	static void registerPgProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url", postgres::getJdbcUrl);
-		registry.add("spring.datasource.username", postgres::getUsername);
-		registry.add("spring.datasource.password", postgres::getPassword);
-	}
 
 	@Autowired
 	private CustomerRepository repository;
