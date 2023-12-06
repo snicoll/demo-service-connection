78341df8ba9164d22515cc9bb4f4355bfe54ff44
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2023-12-06 12:08:25 +0100

    Basic Testcontainers demo
    This commit uses Testcontainers for the test so that a database should
    not be available for it to succeed. We use `@DynamicPropertySource` to
    inject the credentials exposed by the container so that Spring Boot is
    configured to use it.

--- src/test/java/com/example/demo/CustomerRepositoryTests.java
+++ src/test/java/com/example/demo/CustomerRepositoryTests.java
@@ -1,17 +1,33 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Testcontainers(disabledWithoutDocker = true)
 class CustomerRepositoryTests {
+
+	@Container
+	static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.1-alpine");
+
+	@DynamicPropertySource
+	static void registerPgProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Autowired
 	private CustomerRepository repository;
